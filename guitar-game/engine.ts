e9bffc2f0d479b03ae12c1f414db097334f01813
Joel <joel@joelconstantine.com>
2024-06-12 14:20:23 -0500

    Started adding player stats

--- guitar-game/engine.ts
+++ guitar-game/engine.ts
@@ -1,11 +1,33 @@
 import { ECS } from "./ecs"
 import { Stage } from "./graphics/graphics"
+import { EventQueue } from "./setup"
+
+class PlayerStats {
+  score = 0
+  lives = 3
+  updateScore(amount: number) {
+      this.score += amount
+  }
+}
 
 class Engine {
   private prevTimeStamp: number = 0
   public stage: Stage 
-  constructor(public ecs: ECS, stage: Stage) {
+  public events: EventQueue
+  public playerStats: PlayerStats
+  constructor(public ecs: ECS, stage: Stage, events: EventQueue) {
     this.stage = stage
+    this.events = events
+    this.playerStats = new PlayerStats()
+    this.setup()
+  }
+
+  setup() {
+    this.events.on("collectRose", (args) => {
+      this.playerStats.updateScore(1)
+
+      console.log(`rose ${args} collected`)
+  })
   }
 
   update(delta: number) {
