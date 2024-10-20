8c2b40013071248641c690b1d3e78bbaafc7efc0
Joel <joel@joelconstantine.com>
2024-10-20 15:08:17 -0600

    Added settings

--- bone-torch-ts/src/utils/settings.ts
+++ bone-torch-ts/src/utils/settings.ts
@@ -1,3 +1,17 @@
+import { ActionMapping } from "../input/types";
+
+interface ISettings {
+    keyboardMappings: Record<string, ActionMapping>
+}
+
 export const Settings = Object.freeze({
-    
-})
+    keyboardMappings: {
+        gameScreen: {
+            w: ['move_up', 'inventory_scroll_up'],
+            s: ['move_down', 'inventory_scroll_down'],
+            a: ['move_left', 'inventory_scroll_left'],
+            d: ['move_right', 'inventory_scroll_right'],
+            i: ['open_inventory']
+        }
+    }
+} as ISettings)
