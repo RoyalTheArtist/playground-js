fa53557033c6a0b9a4f1a76c872f2e544ea0d1d7
Royal <royal@royaltheartist.com>
2024-10-22 19:10:34 -0600

    Added diagonal directions

--- bone-torch-ts/src/apps/boneTorch/screens/gameScreen/game.handler.ts
+++ bone-torch-ts/src/apps/boneTorch/screens/gameScreen/game.handler.ts
@@ -7,9 +7,42 @@ import { Player } from "@/apps/boneTorch/player"
 
 export class GameInputHandler extends InputHandler { 
     handleInput(input: StandardGameInput): Action | null {
-        if (input.axis.discrete.x !== 0 || input.axis.discrete.y !== 0) {
+        // if (input.axis.discrete.x !== 0 || input.axis.discrete.y !== 0) {
 
-            Player.setNextTurn(new MoveAction(new Vector2D(input.axis.discrete.x, input.axis.discrete.y)))
+        //     Player.setNextTurn(new MoveAction(new Vector2D(input.axis.discrete.x, input.axis.discrete.y)))
+        // }
+
+        // I'm sure there's a better way to do this
+        if (input.actions.get("move_up") === "pressed") {
+            Player.setNextTurn(new MoveAction(new Vector2D(0, -1)))
+        }
+
+        if (input.actions.get("move_down") === "pressed") {
+            Player.setNextTurn(new MoveAction(new Vector2D(0, 1)))
+        }
+
+        if (input.actions.get("move_left") === "pressed") {
+            Player.setNextTurn(new MoveAction(new Vector2D(-1, 0)))
+        }
+
+        if (input.actions.get("move_right") === "pressed") {
+            Player.setNextTurn(new MoveAction(new Vector2D(1, 0)))
+        }
+
+        if (input.actions.get("move_up_left") === "pressed") {
+            Player.setNextTurn(new MoveAction(new Vector2D(-1, -1)))
+        }
+
+        if (input.actions.get("move_up_right") === "pressed") {
+            Player.setNextTurn(new MoveAction(new Vector2D(1, -1)))
+        }
+
+        if (input.actions.get("move_down_left") === "pressed") {
+            Player.setNextTurn(new MoveAction(new Vector2D(-1, 1)))
+        }
+
+        if (input.actions.get("move_down_right") === "pressed") {
+            Player.setNextTurn(new MoveAction(new Vector2D(1, 1)))
         }
 
         if (input.actions.has("open_inventory") && input.actions.get("open_inventory") === "pressed") {
--- bone-torch-ts/src/apps/boneTorch/settings.ts
+++ bone-torch-ts/src/apps/boneTorch/settings.ts
@@ -13,6 +13,10 @@ export const Settings = Object.freeze({
             s: ['move_down', 'inventory_scroll_down'],
             a: ['move_left', 'inventory_scroll_left'],
             d: ['move_right', 'inventory_scroll_right'],
+            q: ['move_up_left', 'inventory_scroll_up_left'],
+            e: ['move_up_right', 'inventory_scroll_up_right'],
+            z: ['move_down_left', 'inventory_scroll_down_left'],
+            c: ['move_down_right', 'inventory_scroll_down_right'],
             i: ['open_inventory']
         },
         mainMenu: {
